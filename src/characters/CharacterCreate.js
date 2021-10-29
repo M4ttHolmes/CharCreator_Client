1b9cfd41686f0ad4d0609ccd81909fba5284bb8e
Irvin Lee <irvin.lee.work@gmail.com>
2021-10-29 10:34:18 -0400

    mergeing stuff

---
+++ src/characters/CharacterCreate.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+
+function CreateCharacter(props) {
+    const [name, setName] = useState("");
+    const [appearance, setAppearance] = useState("");
+    const [personality, setPeronality] = useState("");
+    const [description, setDescription] = useState("");
+    const [background, setBackground] = useState("");
+    const [race, setRace] = useState("");
+    const [charClass, setCharClass] = useState("");
+    const [alignment, setAlignment] = useState("");
+    const [campaignName, setCampaignName] = useState("");
+
+    const postCharacter = (e) => {
+        e.preventDefault();
+
+        let url = 'http://localhost:3000/character/create';
+        fetch(url, {
+            method: 'Post',
+            body: JSON.stringify({
+                name: name,
+                appearance: appearance,
+                personality: personality,
+                description: description,
+                background: background,
+                race: race,
+                charClass: charClass,
+                alignment: alignment,
+                campaignName: campaignName
+            }),
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': props.sessionToken
+            })
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            props.setCreateCharacter(false);
+        })
+        .catch(err => console.log(err))
+    }
+
+    return (
+        <form onSubmit={postCharacter}>
+            <input type='text' onChange={(e) => setName(e.target.value)} value= {name} placeholder="Name" />
+            <input type='text' onChange={(e) => setAppearance(e.target.value)} value= {appearance} placeholder="Appearance" />
+            <input type='text' onChange={(e) => setPeronality(e.target.value)} value= {personality} placeholder="Personality" />
+            <input type='text' onChange={(e) => setDescription(e.target.value)} value= {description} placeholder="Description" />
+            <input type='text' onChange={(e) => setBackground(e.target.value)} value= {background} placeholder="Background" />
+            <input type='text' onChange={(e) => setRace(e.target.value)} value= {race} placeholder="Race" />
+            <input type='text' onChange={(e) => setCharClass(e.target.value)} value= {charClass} placeholder="Character Class" />
+            <input type='text' onChange={(e) => setAlignment(e.target.value)} value= {alignment} placeholder="Alignment" />
+            <input type='text' onChange={(e) => setCampaignName(e.target.value)} value= {campaignName} placeholder="Campaign Name" />
+            <br />
+            <button type='submit'>Submit</button>
+        </form>
+    )
+}
+
+export default CreateCharacter;
